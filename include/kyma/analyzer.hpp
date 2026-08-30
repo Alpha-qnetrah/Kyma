d0df71ed3c4052d21f42dcd314dbf42e347269d8
Ahmed_Mansour <133324agh@gmail.com>
2026-08-31 02:19:03 +0300

    Make REPL analysis state tolerant across snippets

--- include/kyma/analyzer.hpp
+++ include/kyma/analyzer.hpp
@@ -5,8 +5,8 @@
 #include <vector>
 namespace kyma {
 class Analyzer {
-public: std::vector<Diagnostic> analyze(const std::vector<StmtPtr>& program);
-private: std::vector<Diagnostic> errors; struct Scope { std::map<std::string, TypeRef> types; std::map<std::string, bool> mutableBindings; std::shared_ptr<Scope> parent; }; std::shared_ptr<Scope> scope; std::map<std::string, FunctionDecl> functions; std::map<std::string, ClassDecl> classes; TypeRef currentReturn{"void",false,{}}; bool inFunction{false};
+public: std::vector<Diagnostic> analyze(const std::vector<StmtPtr>& program); void setInteractive(bool enabled) { interactive = enabled; }
+private: std::vector<Diagnostic> errors; struct Scope { std::map<std::string, TypeRef> types; std::map<std::string, bool> mutableBindings; std::shared_ptr<Scope> parent; }; std::shared_ptr<Scope> scope; std::map<std::string, FunctionDecl> functions; std::map<std::string, ClassDecl> classes; bool interactive{false}; TypeRef currentReturn{"void",false,{}}; bool inFunction{false};
   void stmt(const StmtPtr&); void warning(const std::string&, SourceLocation); TypeRef expr(const ExprPtr&); TypeRef merge(const TypeRef&, const TypeRef&); bool compatible(const TypeRef&, const TypeRef&); bool defined(const std::string&) const; void error(const std::string&, SourceLocation); Scope* bindingScope(const std::string&) const; bool alwaysReturns(const StmtPtr&) const;
 };
 }
