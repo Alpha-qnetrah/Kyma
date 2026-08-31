8fd19c2ce8a84ea0270f6fd7566f32216e385281
Ahmed_Mansour <133324agh@gmail.com>
2026-08-31 04:33:54 +0300

    Make standard library test portable on Windows

--- tests/stdlib/test_stdlib.cpp
+++ tests/stdlib/test_stdlib.cpp
@@ -5,8 +5,8 @@
 
 int main() {
   auto program = kyma::Parser(kyma::lex("let values = [1, 2]; push(values, 3); print(len(values)); "
-                                        "writeFile(\"/tmp/kyma-stdlib-test.txt\", \"ok\"); "
-                                        "print(readFile(\"/tmp/kyma-stdlib-test.txt\")); "
+                                        "writeFile(\"kyma-stdlib-test.txt\", \"ok\"); "
+                                        "print(readFile(\"kyma-stdlib-test.txt\")); "
                                         "print(processRun(\"true\"));"))
                      .parse();
   assert(kyma::validate(program).empty());
